f4969b48bdf5ad1e94d303817133b99cc635d80e
rychoy <riechan.ye@outlook.com>
2026-01-01 15:03:25 +0000

    Automatically create directory named system's hostname to isolate the pid file.

--- run.sh
+++ run.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-PID_FILE="gpu-dryrun.pid"
+PID_DIR="./$(hostname)"
+PID_FILE="${PID_DIR}/gpu-dryrun.pid"
+LOG_FILE="${PID_DIR}/gpu-dryrun.log"
+GPU_COUNT=$(echo $2 | tr ',' '\n' | wc -l)
 
 case "$1" in
   up)
+    if [ ! -d "$PID_DIR" ]; then
+      mkdir -p "$PID_DIR"
+    fi
+
     if [ -f "$PID_FILE" ]; then
       echo "Process is already running with PID $(cat $PID_FILE)"
       exit 1
     fi
-    CUDA_VISIBLE_DEVICES=$2 nohup python gpu-dryrun.py > gpu-dryrun.log 2>&1 &
+    CUDA_VISIBLE_DEVICES=$2 nohup python gpu-dryrun.py > $LOG_FILE 2>&1 &
     MAIN_PID=$!
     echo $MAIN_PID > "$PID_FILE"
     # Capture child PIDs if any
-    sleep 10
-    CHILD_PIDS=$(pgrep -P $MAIN_PID)
-    if [ -n "$CHILD_PIDS" ]; then
-        for PID in $CHILD_PIDS; do
-            echo $PID >> "$PID_FILE"
-        done
-    fi
+    while true; do
+      CHILD_PIDS=$(pgrep -P $MAIN_PID)
+      CHILD_COUNT=$(echo "$CHILD_PIDS" | wc -w)
+      if [ "$CHILD_COUNT" -eq $((GPU_COUNT + 1)) ]; then
+          for PID in $CHILD_PIDS; do
+              echo $PID >> "$PID_FILE"
+          done
+          break
+      fi
+      sleep 1
+    done
     echo "Started gpu-dryrun.py with PIDs: $MAIN_PID $CHILD_PIDS"
     ;;
   down)
@@ -27,7 +38,10 @@ case "$1" in
           kill "$PID" && echo "Stopped process with PID $PID"
         fi
       done < "$PID_FILE"
-      rm -f "$PID_FILE"
+      rm -f "${PID_FILE}"
+      rm -f "${LOG_FILE}"
+      sleep 1
+      rm -rf "${PID_DIR}"
     else
       echo "No processes are running."
     fi
